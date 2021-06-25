c3f41a8a249c2d96afb2a3f7a4aa36467f2e8b30
bzhumakova <bzhumakova@gmail.com>
2021-06-25 18:37:04 +0600

    Add match name to match path url

--- SportBetProject/MatchTeam/urls.py
+++ SportBetProject/MatchTeam/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns=[
     path('team/',TeamView.as_view()),
-    path('match/',MatchView.as_view())
+    path('match/',MatchView.as_view(),name='match'),
+    path('counter/<int:match_id>/',Counter.as_view())
 ]
