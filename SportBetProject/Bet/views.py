d6da31e07eed451ef88c3d4f1df20b8e59f85387
bzhumakova <bzhumakova@gmail.com>
2021-06-25 18:35:54 +0600

    Add try exceptions for match and profile, add condition if for status of match. Add try exceptions for checking the promo_code validation

--- SportBetProject/Bet/views.py
+++ SportBetProject/Bet/views.py
@@ -6,12 +6,23 @@ from rest_framework.views import APIView
 from .serializers import *
 from MatchTeam.models import Match
 from .service import *
+from userprofile.models import Profile
+
 
 class BetPostView(APIView):
     def post(self,request,*args,**kwargs):
-        match=Match.objects.get(id=kwargs['match_id'])
-        profile=request.user.profile
+        try:
+            match=Match.objects.get(id=kwargs['match_id'])
+        except Match.DoesNotExist:
+            return Response('Does not exist!',status=404)
+        if match.status!='soon':
+            return Response('Match closed or live',status=404)
+        try:
+            profile=request.user.profile
+        except Profile.DoesNotExist:
+            return Response('Login or register!',status=404)
         serializer=BetSerializer(data=request.data)
+
         if serializer.is_valid():
             amount=serializer.data.get('amount')
             result=serializer.data.get('result')
@@ -30,7 +41,14 @@ class TransactionView(APIView):
         if serializer.is_valid():
             amount = serializer.data.get('amount')
             promo_code=serializer.data.get('promo_code')
-            total_amount_wallet(amount, profile)
+            try:
+                check=Promocode.objects.get(code=promo_code).bonus
+                print(check)
+            except Promocode.DoesNotExist:
+                check=0
+            profile.wallet+=amount+check
+            profile.save()
+
             Transaction.objects.create(amount=amount,promo_code=promo_code,profile=profile)
             return Response('Successful!',status=201)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
