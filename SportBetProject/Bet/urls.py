3321a3038593b2782412609ece3fee4eef3523d1
bzhumakova <bzhumakova@gmail.com>
2021-06-25 18:36:41 +0600

    Add bet name to bet path url

--- SportBetProject/Bet/urls.py
+++ SportBetProject/Bet/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns=[
-    path('bet/<int:match_id>/',BetPostView.as_view()),
+    path('bet/<int:match_id>/',BetPostView.as_view(),name='bet'),
     path('trans/', TransactionView.as_view()),
 ]
